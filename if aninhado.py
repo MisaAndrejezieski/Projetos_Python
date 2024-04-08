67f600f81eaba03535f4932ac60b61c3f43a4381
MisaAndrejezieski <mandrejezieski@gmail.com>
2024-04-08 16:28:57 -0300

    Update if aninhado.py

--- if aninhado.py
+++ if aninhado.py
@@ -3,10 +3,19 @@ y = int(input("Digite um número: "))
 z = int(input("Digite um número: ")) 
 
 maior_numero = max(x, y, z)
+menor_numero = min(x, y, z)
+medio_numero = med(x, y, z)
 
 if maior_numero == x:
     print("x é o maior número.")
 elif maior_numero == y:
     print("y é o maior número.")
+else:
+    print("z é o maior número.")
+
+if menor_numero == x:
+    print("x é o menor número.")
+elif maior_numero == y:
+    print("y é o menor número.")
 else:
     print("z é o maior número.")
