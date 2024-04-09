641ba4c16ddba6fa1d0c4d0d9606ab96b44247ef
MisaAndrejezieski <mandrejezieski@gmail.com>
2024-04-09 13:17:47 -0300

    Create recomendar_plano_001.py

---
+++ recomendar_plano_001.py
@@ -0,0 +1,18 @@
+def recomendar_plano(consumo):
+    if consumo <= 10:
+        return "Plano Essencial Fibra - 50Mbps"
+    elif consumo <= 20:
+        return "Plano Prata Fibra - 100Mbps"
+    else:
+        return "Plano Premium Fibra - 300Mbps"
+
+
+try:
+    # Solicita ao usuário que insira o consumo médio mensal de dados:
+    consumo = float(input())
+    # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
+    print(recomendar_plano(consumo))
+except ValueError as e:
+    print(f"Error: {e}")
+
+print('Continuo processando')
