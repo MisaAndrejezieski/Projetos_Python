7574d6c9a0b4aa635b73ae9ebef2436c6c8cc02b
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-09 14:47:43 -0300

    Update telefone.py

--- telefone.py
+++ telefone.py
@@ -5,7 +5,7 @@ print("Digite o número do telefone seguindo o modelo '(42) 9xxxx-xxxx':")
 def validate_numero_telefone(phone_number):
     pattern = r'\(\d{2}\) 9\d{4}-\d{4}'
     if re.match(pattern, phone_number):
-        return 'Número de telefone válido.'
+        return '::: Número de telefone válido.'
     else:
         return 'Número de telefone inválido.'
 
