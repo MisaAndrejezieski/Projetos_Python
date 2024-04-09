3e55a81dafebfe931d6285e605838717435b05c1
MisaAndrejezieski <mandrejezieski@gmail.com>
2024-04-09 12:32:53 -0300

    Update if aninhado.py

--- if aninhado.py
+++ if aninhado.py
@@ -1,12 +1,15 @@
 print("Um if aninhado é apenas um if dentro de outro if.")
 
 numeros = [int(input("Digite um número: ")) for _ in range(3)]
-nomes = ['x', 'y', 'z']
+nomes = ['primeiro numero', 'segundo numero', 'terceiro numero']
 
 maior_numero = max(numeros)
 menor_numero = min(numeros)
 numero_do_meio = sum(numeros) - maior_numero - menor_numero
 
+
+print("==================================================================")
 print(f"{nomes[numeros.index(maior_numero)]} é o maior número: {maior_numero}")
 print(f"{nomes[numeros.index(menor_numero)]} é o menor número: {menor_numero}")
 print(f"{nomes[numeros.index(numero_do_meio)]} é o número do meio: {numero_do_meio}")
+print("==================================================================")
