eba338c6bd24722060cdd41703eb8cbe7330e948
MisaAndrejezieski <mandrejezieski@gmail.com>
2024-04-09 13:57:51 -0300

    Update recomendar_plano_002.py

--- recomendar_plano_002.py
+++ recomendar_plano_002.py
@@ -1,3 +1,4 @@
+plano = "plano de internet"
 def recomendar_plano(nome, consumo):
     if consumo <= 10:
         print(f"{nome}, recomendamos o Plano de 50Mbps por $50,00, nesta promoção.")
@@ -18,7 +19,7 @@ try:
     consumo = float(input())
     # Chama a função recomendar_plano com o nome e o consumo inseridos e imprime o plano recomendado:
 
-    print(".".join("plano de internet".upper))
+    print(".".join(plano.upper))
     print(recomendar_plano(nome.title().center(20,"_"), consumo))
 except ValueError as e:
     print(f"Error: {e}")
