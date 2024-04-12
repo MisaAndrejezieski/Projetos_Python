1c91114da6207b527d74032f892493d6115beca1
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 13:29:12 -0300

    Up

--- trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao_002.py
+++ trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao_002.py
@@ -1,12 +1,19 @@
 def calcular_total(numeros):
     return sum(numeros)
 
-
 def retorna_antecessor_e_sucessor(numero):
     antecessor = numero - 1
     sucessor = numero + 1
 
     return antecessor, sucessor
 
+def antsec():
+    num = int(input("Digite o número: "))  # Corrigido aqui
+    ant = num - 1
+    sec = num + 1
+    print("O antecessor de", num, " é", ant, " e o sucessor de ",num, " é ",sec)
+
 print(calcular_total([10, 20, 34]))  # 64
 print(retorna_antecessor_e_sucessor(10))  # (9, 11)
+
+antsec()
