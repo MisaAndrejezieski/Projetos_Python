6f4e842a1a256c578ed067613045b0eef0ccc3de
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 13:00:32 -0300

    Update 01_retorno_da_funcao.py

--- trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
+++ trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
@@ -2,9 +2,7 @@ def calcular_total(numeros):
     return sum(numeros)
 
 
-
 def retorna_antecessor_e_sucessor(numero):
-    print("Estes são o antecessor e sucessor de ",numero)
     antecessor = numero - 1
     sucessor = numero + 1
 
@@ -12,4 +10,4 @@ def retorna_antecessor_e_sucessor(numero):
 
 
 print(calcular_total([10, 20, 34]))  # 64
-print(retorna_antecessor_e_sucessor, (10))  # (9, 11)
+print(retorna_antecessor_e_sucessor(10))  # (9, 11)
