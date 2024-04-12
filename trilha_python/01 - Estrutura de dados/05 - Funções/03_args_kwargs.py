f458af040d83ec2c6b897ad4b45107529c553a51
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 13:23:33 -0300

    Update 03_args_kwargs.py

--- trilha_python/01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
+++ trilha_python/01 - Estrutura de dados/05 - Funções/03_args_kwargs.py
@@ -1,11 +1,12 @@
 def exibir_poema(data_extenso, *args, **kwargs):
     texto = "\n".join(args)
     meta_dados = "\n".join([f"{chave.title()}: {valor}" for chave, valor in kwargs.items()])
-    mensagem = f"{data_extenso}\n\n{texto}\n\n{meta_dados}"
+    mensagem = f"\n{data_extenso}\n\n{texto}\n\n{meta_dados}"
     print(mensagem)
 
 
 exibir_poema(
+    "Sexta, 12/04/2024."
     "Zen of Python",
     "Beautiful is better than ugly.",
     "Explicit is better than implicit.",
