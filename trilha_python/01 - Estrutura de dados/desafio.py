b36825c28059d49ee0859a8ff404f2129d762242
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 16:44:02 -0300

    Update desafio.py

--- trilha_python/01 - Estrutura de dados/desafio.py
+++ trilha_python/01 - Estrutura de dados/desafio.py
@@ -3,7 +3,7 @@ import textwrap
 
 def menu():
     menu = """\n
-            Banco DESAFIO PYTHON
+            BANCO DESAFIO PYTHON
     ================ MENU ================
     [1]\tDepositar
     [2]\tSacar
