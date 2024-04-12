74f530d7be91a97a1e62f01c2a53f334f5a32454
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 17:09:20 -0300

    Update recomendar_plano.py

--- recomendar_plano.py
+++ recomendar_plano.py
@@ -1,5 +1,3 @@
-print("Digite seu consumo de internet: ")
-
 def recomendar_plano(consumo):
 
     if consumo <= 10:
@@ -15,14 +13,9 @@ def recomendar_plano(consumo):
         return "Plano Premium Fibra - 300Mbps"
 
 
-
-
 # Solicita ao usuário que insira o consumo médio mensal de dados:
 
 consumo = float(input())
-
-
-
 
 # Chama a função recomendar_plano com o consumo inserido e imprime o plano recomendado:
 
