91adf4f5b0d02e83aa6f28950d89a6b53854dc9b
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 13:30:28 -0300

    Update 01_retorno_da_funcao_002.py

--- trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao_002.py
+++ trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao_002.py
@@ -8,10 +8,10 @@ def retorna_antecessor_e_sucessor(numero):
     return antecessor, sucessor
 
 def antsec():
-    num = int(input("Digite o número: "))  # Corrigido aqui
+    num = int(input("Digite o número: "))  # Passando valor para num
     ant = num - 1
     sec = num + 1
-    print("O antecessor de", num, " é", ant, " e o sucessor de ",num, " é ",sec)
+    print("O antecessor de ",num," é ",ant," e o sucessor de ",num," é ",sec)
 
 print(calcular_total([10, 20, 34]))  # 64
 print(retorna_antecessor_e_sucessor(10))  # (9, 11)
