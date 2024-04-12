a1cfe1b0f17135508ead1bb4dfc8b6de5ec405f2
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 12:56:24 -0300

    Update 01_retorno_da_funcao.py

--- trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
+++ trilha_python/01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
@@ -1,7 +1,7 @@
 def calcular_total(numeros):
     return sum(numeros)
 
-numero = input("Digite o número: ")
+
 
 def retorna_antecessor_e_sucessor(numero):
     print("Estes são o antecessor e sucessor de ",numero)
