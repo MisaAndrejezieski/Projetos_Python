17c6eeb1c0f3f1a32d065b2451ac9fbc0914ad4a
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-12 12:18:37 -0300

    Update telefone.py

--- telefone.py
+++ telefone.py
@@ -6,12 +6,13 @@ def validate_numero_telefone(phone_number):
     pattern = r'\(\d{2}\) 9\d{4}-\d{4}'
     if re.match(pattern, phone_number):
         return '::: Número de telefone válido :::'.center(50)
+        print()
     else:
         return '::: Número de telefone inválido :::'.center(50)
 
 
 # Solicita ao usuário que insira um número de telefone e armazena o valor fornecido na variável 'phone_number'.
-phone_number = input()
+phone_number = input("Digite o número do telefone seguindo o modelo '(42) 9xxxx-xxxx':")
 
 
 # Chama a função 'validate_numero_telefone()' com o número de telefone fornecido como argumento e armazena o resultado retornado na variável 'result'.
