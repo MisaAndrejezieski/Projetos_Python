10eb4d81d9c056b73042b7dbd44ec6cc24d05557
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 15:29:35 -0300

    Update teste003.py

--- teste003.py
+++ teste003.py
@@ -5,4 +5,4 @@ print(x)    # retorna [1,4,3].
 
 numbers = (1,2,3,4,5) # criação de uma tupla, tupla são imutaveis. 
 numbers [0] = 10      # tenta trocar na posição 0 o número 1 por 10.
-print(numbers)
+print(numbers)        # como tuplas são imutáveis retorna ERRO.
