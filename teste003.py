7e1dece1915df40f1cc6cf69016b8151470a4af9
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 15:28:56 -0300

    Update teste003.py

--- teste003.py
+++ teste003.py
@@ -1,8 +1,8 @@
 x = [1,2,3] # cria uma lista mutável.
-y = x       # a variavel y recebe x
+y = x       # a variavel y recebe x.
 y[1] = 4    # coloca o valor 4 na posição 1, como começa no 0 troca 2 por 4.
-print(x)    # retorna [1,4,3]
+print(x)    # retorna [1,4,3].
 
-numbers = (1,2,3,4,5) # criação de uma tupla, tupla são imutaveis 
+numbers = (1,2,3,4,5) # criação de uma tupla, tupla são imutaveis. 
 numbers [0] = 10      # tenta trocar na posição 0 o número 1 por 10.
 print(numbers)
