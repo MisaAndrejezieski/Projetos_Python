813c74f93dfd227d31aa93935cc48bff0242b531
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 15:19:33 -0300

    Update teste003.py

--- teste003.py
+++ teste003.py
@@ -1,11 +1,2 @@
-def conditions_func(data):
-    if all(data):
-        return "yes"
-    elif not any(data):
-        return "no"
-    else:
-        return "I dont'n know"
-
-values = [True, False, True, False]       
-
-print(conditions_func(values))
+string = "abcdef"
+print(string[::2])
