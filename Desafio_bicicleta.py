feb113d4c2b81641fe7e3728786da903e9765089
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 12:24:00 -0300

    Update Desafio_bicicleta.py

--- Desafio_bicicleta.py
+++ Desafio_bicicleta.py
@@ -14,22 +14,21 @@ class bicicleta:
 
     def correr(self):
         print("vrum.........")    
+    def __str__(self):
+        return f"bicicleta: Cor = {self.cor}, modelo = {self.modelo}, ano = {self.ano}, valor = {self.valor}"
 
 
 #b1 = bicicleta("vermelha", "caloi", 2022, 600)
 
-b1.buzina()
-b1.correr()
-b1.parar()
-print(b1.cor, b1.modelo, b1.ano, b1.valor)
+#b1.buzina()
+#b1.correr()
+#b1.parar()
+#print(b1.cor, b1.modelo, b1.ano, b1.valor)
 
 #b2 = bicicleta("verde","monark",2000,1890)
 b3 = bicicleta("azul","bmx",2012,1500)
 #bicicleta.buzina(b2)
 #b2.correr()
 #print(b2.cor, b2.modelo, b2.ano, b2.valor)
-
-def __str__(self):
-    return f"bicicleta: {self.cor},{self.modelo},{self.ano},{self.valor}"
 
 print(b3)
