360e4e3dceaf8eb98ccd12d55193a42f4ad9179c
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 11:21:05 -0300

    Create Desafio_bicicleta.py

---
+++ Desafio_bicicleta.py
@@ -0,0 +1,11 @@
+class bicicleta:
+    def __init__(self, cor, modelo, ano, valor):
+        self.cor = cor
+        self.modelo = modelo
+        self.ano = ano
+        self.valor = valor
+
+
+b1 = bicicleta("vermelha", "caloi", 2022, 600)
+
+#print(b1)
