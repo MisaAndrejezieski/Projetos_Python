7562fa8be3ca8b2a4bf327c6ccae542581848363
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 15:26:30 -0300

    Update teste003.py

--- teste003.py
+++ teste003.py
@@ -1,2 +1,8 @@
-string = "abcdefghijklmnopqrstuvyxwz"
-print(string[::5])
+x = [1,2,3] # cria uma lista mutável
+y = x       
+y[1] = 4    # coloca o valor 4 na posição 1, como começa no 0 troca 2 por 4.
+print(x)
+
+numbers = (1,2,3,4,5) # tupla são imutaveis
+numbers [0] = 10
+print(numbers)
