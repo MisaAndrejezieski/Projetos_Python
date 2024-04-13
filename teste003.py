522b1feca7752d04d6d7a7d9ca628d063ebbcc27
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 15:16:39 -0300

    Update teste003.py

--- teste003.py
+++ teste003.py
@@ -1,1 +1,11 @@
-   
+def conditions_func(data):
+    if all(data):
+        return "yes"
+    elif not any(data):
+        return "no"
+    else:
+        return "I dont'n know"
+
+values = [True, False, True, False]       
+
+print(conditions_func(values))
