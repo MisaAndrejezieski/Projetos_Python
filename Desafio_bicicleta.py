01012af7b65ddd66ebc0a4a0ef4ed07580feb94a
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-13 12:38:52 -0300

    Update Desafio_bicicleta.py

--- Desafio_bicicleta.py
+++ Desafio_bicicleta.py
@@ -13,23 +13,25 @@ class bicicleta:
         print("Bicicleta parada.")
 
     def correr(self):
-        print("vrum.........")    
+        print("vrum.........")
+
     def __str__(self):
         print("-------------------------------")
         return f"bicicleta: Cor = {self.cor}, modelo = {self.modelo}, ano = {self.ano}, valor = {self.valor}"
 
+    def __str__(self):
+        return f"{self. __class__.__name__}:{[ f'{chave} = {valor}' for chave, valor in self.__dict__.items()]}"
 
-#b1 = bicicleta("vermelha", "caloi", 2022, 600)
+b1 = bicicleta("vermelha", "caloi", 2022, 600)
+b2 = bicicleta("verde","monark",2000,1890)
+b3 = bicicleta("azul","bmx",2012,1500)
 
 #b1.buzina()
 #b1.correr()
 #b1.parar()
 #print(b1.cor, b1.modelo, b1.ano, b1.valor)
-
-#b2 = bicicleta("verde","monark",2000,1890)
-b3 = bicicleta("azul","bmx",2012,1500)
 #bicicleta.buzina(b2)
 #b2.correr()
 #print(b2.cor, b2.modelo, b2.ano, b2.valor)
-
+print(b2)
 print(b3)
