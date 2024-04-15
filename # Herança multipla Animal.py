51556ea2a07384c16ed2681aea74246d310f09ef
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-15 18:39:54 -0300

    Update # Herança multipla Animal.py

--- # Herança multipla Animal.py
+++ # Herança multipla Animal.py
@@ -5,7 +5,7 @@ class Animal:
 
     def __str__(self):
         return f"{self. __class__.__name__}:{', '.join([ f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
-    
+     
 
 class Mamifero(Animal):
     def __init__(self, cor_pelo, **Kw):
@@ -17,11 +17,11 @@ class Ave(Animal):
     def __init__(self, cor_bico, **Kw):
         self.cor_bico = cor_bico
         super(). __init__(**Kw)
-        
 
-    
 
-    
+
+
+ 
 class Cachorro(Mamifero):
     pass
 
