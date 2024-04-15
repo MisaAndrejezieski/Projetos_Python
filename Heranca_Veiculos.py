81f5810455f5c75259e37397307900e72096c0e9
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-15 17:03:13 -0300

    Create Heranca_Veiculos.py

---
+++ Heranca_Veiculos.py
@@ -0,0 +1,18 @@
+class veiculos:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando o motor")
+
+class Motocicleta(veiculos):
+    pass
+
+class Carro(veiculos):
+    pass
+
+class Caminhao(veiculos):
+    pass
+
