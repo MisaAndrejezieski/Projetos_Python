060b16e67c5ecb858d8e5da239922e2a118e5014
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-15 17:49:30 -0300

    Update # Herança multipla Animal.py

--- # Herança multipla Animal.py
+++ # Herança multipla Animal.py
@@ -1,3 +1,20 @@
 # Herança multipla Animal
-class Animal():
-    
+class Animal:
+    pass
+
+class Mamifero(Animal):
+    pass
+
+class Cachorro(Mamifero):
+    pass
+
+class Gato(Mamifero):
+    pass
+
+class Leao(Mamifero):
+    pass
+
+class Onitorico(Animal, Mamifero):
+    pass
+
+
