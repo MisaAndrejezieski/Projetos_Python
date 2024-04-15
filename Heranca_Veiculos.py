dc48f50cef237a839048332ed58f0e487d79c5e1
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-15 17:18:38 -0300

    Update Heranca_Veiculos.py

--- Heranca_Veiculos.py
+++ Heranca_Veiculos.py
@@ -8,7 +8,7 @@ class Veiculos:
         print("Ligando o motor")
 
     def __str__(self):
-        
+        return f"{self. __class__.__name__}:{', '.join([ f'{chave} = {valor}' for chave, valor in self.__dict__.items()])}"
 
 class Motocicleta(Veiculos):
     pass
