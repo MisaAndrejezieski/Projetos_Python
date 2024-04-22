a6487e5814660141b21937f3e7061d9ca7d37b57
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-22 17:33:04 -0300

    up

--- BD_001sqlite3.py
+++ BD_001sqlite3.py
@@ -7,28 +7,34 @@ conexao = sqlite3.connect('banco_de_dados.db')
 cursor = conexao.cursor()
 
 # Crie uma nova tabela
-cursor.execute('''
-    CREATE TABLE clientes (
-        id INTEGER PRIMARY KEY,
-        nome TEXT,
-        email TEXT,
-        endereço TEXT
-    )
-''')
+# cursor.execute('''
+#     CREATE TABLE clientes (
+#         id INTEGER PRIMARY KEY,
+#         nome TEXT,
+#         email TEXT,
+#         endereço TEXT
+#     )
+# ''')
 
 # Insira alguns dados na tabela
-cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('João', 'joao@email.com', 'Rua dos Joãozinhos'))
-cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Maria', 'maria@email.com', 'Rua das Mariazinhas'))
-cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Ana', 'ana@email.com', 'Rua das Aninhas'))
-cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Pedro', 'pedro@email.com', 'Rua dos Pedrinhos'))
-cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Joana', 'joana@email.com', 'Rua das Joaninhas'))
-cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Luana', 'luana@email.com', 'Rua das Cachorras'))
+# cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('João', 'joao@email.com', 'Rua dos Joãozinhos'))
+# cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Maria', 'maria@email.com', 'Rua das Mariazinhas'))
+# cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Ana', 'ana@email.com', 'Rua das Aninhas'))
+# cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Pedro', 'pedro@email.com', 'Rua dos Pedrinhos'))
+# cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Joana', 'joana@email.com', 'Rua das Joaninhas'))
+# cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Luana', 'luana@email.com', 'Rua das Cachorras'))
+
+#cursor.execute("UPDATE clientes SET endereço = ? WHERE nome = ?", ('Rua das Cadelas', 'Luana'))
+
+cursor.execute("SELECT * FROM clientes WHERE id = 6", ())
+cliente = cursor.fetchone()
+print(cliente)
 
 # Imprima os dados dos clientes
-cursor.execute("SELECT * FROM clientes")
-clientes = cursor.fetchall()
-for cliente in clientes:
-    print(cliente)
+# cursor.execute("SELECT * FROM clientes")
+# clientes = cursor.fetchall()
+# for cliente in clientes:
+#     print(cliente)
 
 # Salve as alterações
 conexao.commit()
