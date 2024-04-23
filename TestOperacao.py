924464b3c58f7ad2264d1e52ca7ffa71645ebf1b
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-23 09:17:55 -0300

    Update TestOperacao.py

--- TestOperacao.py
+++ TestOperacao.py
@@ -2,7 +2,8 @@ import time
 from datetime import date, datetime
 
 data_hora = datetime(2024, 4, 23, 10, 30, 20)
-print(date)
+print(datetime.today())
+print(date.today())
 print(data_hora)
 
 def soma(a, b):
