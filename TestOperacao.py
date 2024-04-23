15575000c912b67102aae9b5819ac0898e86b0b9
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-23 09:24:27 -0300

    Update TestOperacao.py

--- TestOperacao.py
+++ TestOperacao.py
@@ -1,10 +1,10 @@
 import time
 import datetime 
 
-data_hora = datetime(2024, 4, 23, 10, 30, 20)
-print(datetime.today())
-print(date.today())
-print(data_hora)
+# data_hora = datetime(2024, 4, 23, 10, 30, 20)
+# print(datetime.today())
+# print(date.today())
+# print(data_hora)
 
 d = datetime.datetime(2024, 4, 23, 13, 45)
 d = d + datetime.timedelta(weeks=1)
