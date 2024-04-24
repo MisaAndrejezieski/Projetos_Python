a627df639cdf5743c9bc82b2cfb60864d0b0b3c2
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-04-24 17:41:00 -0300

    Update BD_001sqlite3.py

--- BD_001sqlite3.py
+++ BD_001sqlite3.py
@@ -6,15 +6,15 @@ conexao = sqlite3.connect('banco_de_dados.db')
 # Crie um cursor para executar comandos SQL
 cursor = conexao.cursor()
 
-# Crie uma nova tabela
-cursor.execute('''
-    CREATE TABLE clientes (
-        id INTEGER PRIMARY KEY,
-        nome TEXT,
-        email TEXT,
-        endereço TEXT
-    )
-''')
+# # Crie uma nova tabela
+# cursor.execute('''
+#     CREATE TABLE clientes (
+#         id INTEGER PRIMARY KEY,
+#         nome TEXT,
+#         email TEXT,
+#         endereço TEXT
+#     )
+# ''')
 
 # Insira alguns dados na tabela
 # cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('João', 'joao@email.com', 'Rua dos Joãozinhos'))
@@ -24,21 +24,17 @@ cursor.execute('''
 # cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Joana', 'joana@email.com', 'Rua das Joaninhas'))
 # cursor.execute("INSERT INTO clientes (nome, email, endereço) VALUES (?, ?, ?)", ('Luana', 'luana@email.com', 'Rua das Cachorras'))
 
-
-novo_endereco = 'Rua das Cadelas'  # Substitua 'Rua Nova' pelo novo endereço
-cursor.execute("UPDATE clientes SET endereço = ? WHERE nome = ?", (novo_endereco, 'Luana'))
-
 #cursor.execute("UPDATE clientes SET endereço = ? WHERE nome = ?", ('Rua das Cadelas', 'Luana'))
 
-cursor.execute("SELECT * FROM clientes WHERE id = 6", ())
-cliente = cursor.fetchone()
-print(cliente)
+# cursor.execute("SELECT * FROM clientes WHERE id = 6", ())
+# cliente = cursor.fetchone()
+# print(cliente)
 
 # Imprima os dados dos clientes
-# cursor.execute("SELECT * FROM clientes")
-# clientes = cursor.fetchall()
-# for cliente in clientes:
-#     print(cliente)
+cursor.execute("SELECT * FROM clientes")
+clientes = cursor.fetchall()
+for cliente in clientes:
+     print(cliente)
 
 # Salve as alterações
 conexao.commit()
