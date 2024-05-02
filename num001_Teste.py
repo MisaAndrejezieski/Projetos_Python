94467bc4f8352a92944aef3bea314d7b3a42ee92
MisaAndrejezieski <misaelandrejezieski130982@outlook.com>
2024-05-02 15:50:59 -0300

    Update num001_Teste.py

--- num001_Teste.py
+++ num001_Teste.py
@@ -1,2 +1,8 @@
-numeros = [n**2 if n > 50 else n for n in range(100) if n % 2 == 0]
-print(numeros)
+# Lista original de números pares elevados ao quadrado se maior que 2, ou o próprio número se 2 ou menos
+numeros_pares = [n**2 if n > 2 else n for n in range(10) if n % 2 == 0]
+
+# Criando uma lista para armazenar os números ímpares
+numeros_impares = [n for n in range(10) if n % 2 != 0]
+
+print("Números pares:", numeros_pares)
+print("Números ímpares:", numeros_impares)
